52d8e71ec6c57c4bf1ad272befc77459af454075
hari <hari@debian.sony>
2023-08-10 01:53:26 +0530

    failed expiriments

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+// cool fade effects - thanks to fireship
 const observer = new IntersectionObserver((entries) => {
 	entries.forEach((entry) => {
 		if (entry.isIntersecting) {
@@ -12,3 +13,18 @@ const observer = new IntersectionObserver((entries) => {
 const hiddenElements = document.querySelectorAll('.hidden')
 
 hiddenElements.forEach((el) => observer.observe(el));
+
+// dynamic images on homepage
+// dynImg = document.querySelectorAll('.ad__img')
+// n = 1
+// dynImg[0].style['background-position'] = "left"
+// dynImg[1].style['background-position'] = "90% 50%"
+// const updateImg = () => {
+// 	console.log(dynImg)
+// 	for(i = 0; i < 2; i++) {
+// 		dynImg[i].style['background'] = `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('images/img-${n}.jpg')`
+// 	}
+// 	n == 1 ? n++ : n = 1
+// }
+
+// setInterval(updateImg, 2500)
